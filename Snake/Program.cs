74c046a2cbf332337d92cd88d921757d18eaf5ef
Ivan Khudyakov <ivanch83ya@yandex.ru>
2022-05-15 19:08:35 +0300

    New branch lesson_2/Functions

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,8 +6,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            
         }
     }
 }
